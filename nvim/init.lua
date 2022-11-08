84c34cd60be50de1d9bfd5ffe22124c9dbf3a058
Reece Russell <rerussell@protonmail.com>
2022-11-08 08:06:46 +0200

    Tab completion for coc.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,5 +4,6 @@ vim.o.relativenumber = true
 vim.o.wrap = true
 
 vim.api.nvim_set_keymap('i', 'jj', '<ESC>', { noremap = true })
+vim.api.nvim_set_keymap('i', '<Tab>', "pumvisible() ? '<C-n>' : '<Tab>'", { noremap = true, silent = true, expr = true, replace_keycodes = false })
 
 require("plugins")
