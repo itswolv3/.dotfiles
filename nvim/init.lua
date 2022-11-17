09a05a1edcae4f7d7a845a498c85987ea26caf7d
Reece Russell <rerussell@protonmail.com>
2022-11-17 14:37:15 +0200

    Fixed errors from init.lua and removed theming from tmux.conf

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,9 @@
 vim.o.cursorline = true
 vim.o.number = true
 vim.o.relativenumber = true
-vim.o.wrap = true
+vim.o.wrap = false
 
 vim.api.nvim_set_keymap('i', 'jj', '<ESC>', { noremap = true })
-vim.api.nvim_set_keymap('i', '<Tab>', "pumvisible() ? '<C-n>' : '<Tab>'", { noremap = true, silent = true, expr = true, replace_keycodes = false })
+vim.api.nvim_set_keymap('i', '<Tab>', "pumvisible() ? '<C-n>' : '<Tab>'", { noremap = true, silent = true, expr = true })
 
 require("plugins")
