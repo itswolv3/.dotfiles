713255e40d2bee0e427522ff97a8ec746568f0dd
Reece Russell <rerussell@protonmail.com>
2022-11-28 21:45:54 +0200

    added theming and some other changes

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,13 @@
+-- vim.o.termguicolors = true
 vim.o.cursorline = true
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.wrap = false
 
-vim.api.nvim_set_keymap('i', 'jj', '<ESC>', { noremap = true })
+vim.api.nvim_set_keymap('i', 'jk', '<ESC>', { noremap = true })
 vim.api.nvim_set_keymap('i', '<Tab>', "pumvisible() ? '<C-n>' : '<Tab>'", { noremap = true, silent = true })
 
 require("plugins")
+require("colors.onedark")
+
+vim.cmd("colorscheme onedark")
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,4 +1,5 @@
 return require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim'
-	use 'neoclide/coc.nvim'
+	use 'wbthomason/packer.nvim' --Packer
+	use 'neoclide/coc.nvim' -- Coc 
+	use 'navarasu/onedark.nvim' -- Theme One Dark
 end)
