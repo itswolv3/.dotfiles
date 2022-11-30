7ac5f7be0ff8634338a3e2bd6f8f519a2c926d5a
Reece Russell <rerussell@protonmail.com>
2022-11-30 08:43:56 +0200

    Plugins and config changes to nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,13 +1,12 @@
--- vim.o.termguicolors = true
 vim.o.cursorline = true
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.wrap = false
 
 vim.api.nvim_set_keymap('i', 'jk', '<ESC>', { noremap = true })
-vim.api.nvim_set_keymap('i', '<Tab>', "pumvisible() ? '<C-n>' : '<Tab>'", { noremap = true, silent = true })
 
 require("plugins")
 require("colors.onedark")
+require("lualine").setup()
 
 vim.cmd("colorscheme onedark")
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -2,4 +2,11 @@ return require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim' --Packer
 	use 'neoclide/coc.nvim' -- Coc 
 	use 'navarasu/onedark.nvim' -- Theme One Dark
+	use 'maxmellon/vim-jsx-pretty'-- React Syntax highlighting
+	use 'ervandew/supertab' -- Tab Completion
+	use 'tpope/vim-fugitive' -- Git wrapper
+	use {
+  		'nvim-lualine/lualine.nvim',
+  		requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+	}
 end)
