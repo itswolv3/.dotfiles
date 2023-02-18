0f6349c8679b75adc0232fb63dc1bdb87835e7cf
Reece Russell <rerussell@protonmail.com>
2023-02-18 21:41:37 +0200

    re-organised nvim config

---
+++ nvim/lua/itswolv3/init.lua
@@ -0,0 +1,3 @@
+require("itswolv3.remap")
+require("itswolv3.set")
+require("itswolv3.packer")
---
+++ nvim/lua/itswolv3/packer.lua
@@ -0,0 +1,48 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
+    
+    -- Nvim additions
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.1',
+		-- or                            , branch = '0.1.x',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+
+	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+	use 'nvim-treesitter/playground'
+	use 'ThePrimeagen/harpoon'
+	use 'mbbill/undotree'
+	use 'tpope/vim-fugitive'
+
+    -- Code completion
+    use 'ervandew/supertab'
+    use 'neoclide/coc.nvim'
+    use 'ervandew/supertab'
+   
+    -- Syntax Highlighting
+    use 'maxmellon/vim-jsx-pretty'-- React Syntax highlighting
+	use 'pangloss/vim-javascript' --JS Syntax
+	use 'mxw/vim-jsx' -- React Syntax
+    use 'posva/vim-vue' -- Vue plugin
+
+    -- Visual Stuff
+    use({
+        'rose-pine/neovim',
+        as = 'rose-pine',
+        config = function()
+            require("rose-pine").setup()
+            vim.cmd('colorscheme rose-pine')
+        end
+    })
+   	use {
+		'nvim-lualine/lualine.nvim',
+		requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+	}
+
+end)
