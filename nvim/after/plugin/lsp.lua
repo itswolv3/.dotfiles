03365011bc018bb76f3827b36a0c3739578a4316
Reece Russell <rerussell@protonmail.com>
2023-11-02 22:31:46 +0200

    Modified - lsp.lua

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,28 +1,20 @@
 local lsp = require("lsp-zero")
 
-lsp.preset("recommended")
-
-lsp.ensure_installed({
-	"tsserver",
-	"eslint",
+require("mason").setup()
+require("mason-lspconfig").setup({
+	ensure_installed = {"tsserver", }
+	
 })
+
+lsp.preset("recommended")
 
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<Tab>'] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
-})
 
 lsp.set_preferences({
   sign_icons = { }
 })
 
-lsp.setup_nvim_cmp({
-  mapping = cmp_mappings
-})
 
 lsp.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
